8348023726f2839eac9a8aa67d35786bdb0d8a60
m05e5 <calebmoses1111@gmail.com>
2021-07-29 12:33:28 +0100

    add inner html

--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -1,3 +1,10 @@
 import TaskUtils from '../taskUtils';
 
 jest.mock('../taskUtils');
+
+document.body.innerHTML = '<div>'
++ ' <ul id="task-list">'
++ '</ul>'
++ '<button id="add-task" />'
++ '</div>';
+
