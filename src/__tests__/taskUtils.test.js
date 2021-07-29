938de3df44197016b0920db543b6a534904fe6a2
m05e5 <calebmoses1111@gmail.com>
2021-07-29 12:34:12 +0100

    add displayTasks function

--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -8,3 +8,21 @@ document.body.innerHTML = '<div>'
 + '<button id="add-task" />'
 + '</div>';
 
+const displayTasks = (arr) => {
+  const ul = document.querySelector('ul');
+  arr.forEach((e) => {
+    const li = document.createElement('li');
+
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.className = 'check';
+    checkBox.checked = e.completed;
+
+    const label = document.createElement('label');
+
+    label.appendChild(document.createTextNode(e.description));
+    li.appendChild(checkBox);
+    li.appendChild(label);
+    ul.appendChild(li);
+  });
+};
