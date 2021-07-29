4e3cea1856d7a6d0ce62ef70d01b4995e8e6d191
enkog <nkirukaogu@gmail.com>
2021-07-29 12:36:55 +0100

    Add tests to add an element to the list

--- src/__tests__/taskUtils.test.js
+++ src/__tests__/taskUtils.test.js
@@ -26,3 +26,25 @@ const displayTasks = (arr) => {
     ul.appendChild(li);
   });
 };
+
+describe('add exactly one <li> element to the list in the DOM', () => {
+  const taskUtil = new TaskUtils();
+  const task = { description: 'Task 1', completed: false, index: 1 };
+  taskUtil.addTask(task);
+  const tasks = taskUtil.getTasks();
+  displayTasks(tasks);
+
+  test('that the length of the list is 1', () => {
+    const list = document.querySelectorAll('#task-list li');
+    expect(list).toHaveLength(1);
+  });
+
+  test('that the length of the list is not zero', () => {
+    const list = document.querySelectorAll('#task-list li');
+    expect(list).not.toHaveLength(0);
+  });
+
+  test('that the description is Task 1 ', () => {
+    expect(tasks[0].description).toEqual('Task 1');
+  });
+});
